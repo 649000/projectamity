ff9d0c3bb95d6d2954b7cc3eee54c92d282d08f2
yuanjie91 <yuanjie91@8bc94c53-19bf-512c-9016-643959c5a65d>
2010-08-17 09:24:17 +0000

    MessageMobileController updated with closures to deal with interaction with the Android app's PM module.

--- trunk/ProjectAmity/grails-app/controllers/app/MessageMobileController.groovy
+++ trunk/ProjectAmity/grails-app/controllers/app/MessageMobileController.groovy
@@ -2,6 +2,7 @@ package app
 
 import java.util.*
 import java.text.*
+import grails.converters.JSON
 
 class MessageMobileController
 {
@@ -117,6 +118,149 @@ class MessageMobileController
         {
             render 'N'
         }
+    }
+
+    def loadInbox =
+    {
+        def user = params.user
+        def timeStamp = params.timeStamp
+        def newMessages
+
+        // Check if recipient exists
+        if( user != null )
+        {
+            user = Resident.findByUserid(user)
+            newMessages = Message.createCriteria().list
+            {
+                and
+                {
+                    eq("receiver", user)
+                    SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd hh:mm:ss");
+                    timeStamp = sdf.parse(timeStamp);
+                    SimpleDateFormat checker = new SimpleDateFormat("dd MMMM yyyy");
+                    println( checker.format(timeStamp) )
+                    // ge("timeStamp", timeStamp )
+                    eq("isRead", false)
+                    order("timeStamp", "desc")
+                }
+            }
+            println( newMessages.size() )
+            println( newMessages )
+        }
+
+        if( newMessages.size() > 0 )
+        {
+            render newMessages as JSON
+        }
+        else
+        {
+            render 'N'
+        }
+    }
+
+    def viewMsg =
+    {
+        def messageID = params.messageID
+        def message
+
+        message = Message.findById(messageID)
+        println( message )
+
+        if( message != null )
+        {
+            def list = [ message, message.sender.name, message.receiver.name, message.sender.userid, message.receiver.userid ]
+            render list as JSON
+        }
+        else
+        {
+            render 'N'
+        }
+    }
+
+    def sendFromAndroid =
+    {
+        def sender = params.sender
+        def receiver = params.receiver
+        def subject = params.subject
+        def message = params.message
+        println( sender )
+
+        def errors = ''
+
+        // Check if recipient exists
+        if( receiver == null || Resident.findByUserid(receiver) == null )
+        {
+            errors += 'Your recipient\'s User ID|'
+        }
+        else
+        {
+            receiver = Resident.findByUserid(receiver)
+        }
+
+        // Check if the subject is specified.
+        if( subject == null || subject.trim().equals("") )
+        {
+            errors += 'Subject|'
+        }
+
+        // Check if the message is specified.
+        if( message == null || message.trim().equals("") )
+        {
+            errors += 'Message|'
+        }
+
+        if( errors.length() > 0 )
+        {
+            errors = '@|' + errors.substring(0, (errors.length() - 1) )
+            // If there are missing fields, throw back the error message.
+            render errors
+        }
+        else if(sender != null)
+        {
+            def newMessage = new Message()
+            newMessage.sender = Resident.findByUserid(sender)
+            newMessage.receiver = receiver
+            if( params.newMessage != null )
+            {
+                if( params.newMessage.equalsIgnoreCase("false") )
+                {
+                    newMessage.subject = "RE: " + subject
+                }
+                else
+                {
+                    newMessage.subject =  subject
+                }
+            }
+            else
+            {
+                newMessage.subject =  subject
+            }
+            newMessage.message = message
+            newMessage.timeStamp = new Date()
+            newMessage.isRead = false
+
+            if( newMessage.save() )
+            {
+                render 'T'
+            }
+            else
+            {
+                println( newMessage.errors )
+                render 'F'
+            }
+        }
+    }
+
+    def markAsRead =
+    {
+        def messageID = params.messageID
+        println( "SERVER IS HERE" )
+
+        def m = Message.findById(messageID)
+
+        m.isRead = true
+
+        render 'T'
     }
 
 }
