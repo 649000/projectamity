91c3e9b647453f4c5949511630390f7745545eb2
yuanjie91 <yuanjie91@8bc94c53-19bf-512c-9016-643959c5a65d>
2010-08-31 05:51:17 +0000

    New CabpoolListing and updated Resident domain classes.

--- trunk/ProjectAmity/grails-app/domain/app/Resident.groovy
+++ trunk/ProjectAmity/grails-app/domain/app/Resident.groovy
@@ -10,7 +10,7 @@ class Resident {
     String userid
     String password
     static hasMany = [ report: Report, indoorReport: IndoorReport, sentMessages : Message, receivedMessages : Message ]
-    static hasOne = [ carpoolListing : CarpoolListing ]
+    static hasOne = [ carpoolListing : CarpoolListing, cabpoolListing : CabpoolListing ]
     static mappedBy = [ sentMessages : 'sender', receivedMessages : 'receiver' ]
 
     static constraints = {
