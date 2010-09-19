adc6344927abdb6efc5978f63ec18ed284b5bd46
yuanjie91 <yuanjie91@8bc94c53-19bf-512c-9016-643959c5a65d>
2010-09-19 02:58:11 +0000

    Updated CarpoolListing, CarpoolRequest and Message domain classes.

--- ProjectAmity/grails-app/domain/app/CarpoolListing.groovy
+++ ProjectAmity/grails-app/domain/app/CarpoolListing.groovy
@@ -1,10 +1,50 @@
 package app
 
-class CarpoolListing {
+class CarpoolListing
+{
 
-    static constraints = {
+    static constraints =
+    {
+        departureMondayTime(nullable:true)
+        departureMondayInterval(nullable:true)
+        departureTuesdayTime(nullable:true)
+        departureTuesdayInterval(nullable:true)
+        departureWednesdayTime(nullable:true)
+        departureWednesdayInterval(nullable:true)
+        departureThursdayTime(nullable:true)
+        departureThursdayInterval(nullable:true)
+        departureFridayTime(nullable:true)
+        departureFridayInterval(nullable:true)
+        departureSaturdayTime(nullable:true)
+        departureSaturdayInterval(nullable:true)
+        departureSundayTime(nullable:true)
+        departureSundayInterval(nullable:true)
+
+        returnMondayTime(nullable:true)
+        returnMondayInterval(nullable:true)
+        returnTuesdayTime(nullable:true)
+        returnTuesdayInterval(nullable:true)
+        returnWednesdayTime(nullable:true)
+        returnWednesdayInterval(nullable:true)
+        returnThursdayTime(nullable:true)
+        returnThursdayInterval(nullable:true)
+        returnFridayTime(nullable:true)
+        returnFridayInterval(nullable:true)
+        returnSaturdayTime(nullable:true)
+        returnSaturdayInterval(nullable:true)
+        returnSundayTime(nullable:true)
+        returnSundayInterval(nullable:true)
+
+        oneOffDepartureTime(nullable:true)
+        oneOffDepartureInterval(nullable:true)
+        oneOffReturnTime(nullable:true)
+        oneOffReturnInterval(nullable:true)
     }
-    static belongsTo = [resident: Resident]
+    
+    static belongsTo = [ resident: Resident ]
+    static hasMany = [ carpoolRequests : CarpoolRequest ]
+
+    String riderType // driver, passenger or cabpooler?
 
     String startAddress
     String startLatitude
@@ -14,11 +54,44 @@ class CarpoolListing {
     String endLatitude
     String endLongitude
 
-    String departureTime
-    String returnTime
+    String tripType // commute or oneOff?
 
-    String frequency // which days of the week?
-    String type // type of person the Resident is --> LOOKING FOR <-- driver or passenger or cab pooling
-    String status // pending or accepted?
+    String departureMondayTime
+    int departureMondayInterval
+    String departureTuesdayTime
+    int departureTuesdayInterval
+    String departureWednesdayTime
+    int departureWednesdayInterval
+    String departureThursdayTime
+    int departureThursdayInterval
+    String departureFridayTime
+    int departureFridayInterval
+    String departureSaturdayTime
+    int departureSaturdayInterval
+    String departureSundayTime
+    int departureSundayInterval
+
+    String returnMondayTime
+    int returnMondayInterval
+    String returnTuesdayTime
+    int returnTuesdayInterval
+    String returnWednesdayTime
+    int returnWednesdayInterval
+    String returnThursdayTime
+    int returnThursdayInterval
+    String returnFridayTime
+    int returnFridayInterval
+    String returnSaturdayTime
+    int returnSaturdayInterval
+    String returnSundayTime
+    int returnSundayInterval
+
+    Date oneOffDepartureTime // consists of year, month, day, hour, minute
+    int oneOffDepartureInterval
+    Date oneOffReturnTime // consists of year, month, day, hour, minute
+    int oneOffReturnInterval
+
+    String notes // additional notes
+    String status // active or inactive?
 
 }
--- ProjectAmity/grails-app/domain/app/Message.groovy
+++ ProjectAmity/grails-app/domain/app/Message.groovy
@@ -14,6 +14,7 @@ class Message
 
     static constraints =
     {
+        message(maxSize:5000)
     }
 
 }
