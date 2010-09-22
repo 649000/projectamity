84aea563c06f7423cdf08e0a99052e8d25d57ddf
hairiandy <hairiandy@8bc94c53-19bf-512c-9016-643959c5a65d>
2010-09-22 04:49:53 +0000

    Added in overflow for content

--- trunk/ProjectAmity/grails-app/domain/app/BarterRequest.groovy
+++ trunk/ProjectAmity/grails-app/domain/app/BarterRequest.groovy
@@ -3,11 +3,15 @@ package app
 class BarterRequest {
 
     static constraints = {
+        partyonename(nullable:true)
+        partyonename(nullable:true)
     }
 
     String barterAction
     String partyone
     String partytwo
+    String partyonename
+    String partytwoname
     String requestStatus
     String involvedItems
 }
