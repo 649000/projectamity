71ed36403c5a71e6846d7447aed7c49db51d2293
nazri610@gmail.com <nazri610@gmail.com@8bc94c53-19bf-512c-9016-643959c5a65d>
2010-09-24 06:43:09 +0000

    Added Icon for Consumer App.

--- trunk/ProjectAmityAndroidConsumer/src/org/me/projectamityandroid/R.java
+++ trunk/ProjectAmityAndroidConsumer/src/org/me/projectamityandroid/R.java
@@ -14,21 +14,22 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int cabpoolmapbluepin=0x7f020000;
-        public static final int cabpoolmapredpin=0x7f020001;
-        public static final int cabpoolmapyellowpin=0x7f020002;
-        public static final int ic_tab_artists=0x7f020003;
-        public static final int ic_tab_artists_grey=0x7f020004;
-        public static final int ic_tab_artists_white=0x7f020005;
-        public static final int ic_tab_car=0x7f020006;
-        public static final int ic_tab_car_grey=0x7f020007;
-        public static final int ic_tab_car_white=0x7f020008;
-        public static final int ic_tab_mail=0x7f020009;
-        public static final int ic_tab_mail_grey=0x7f02000a;
-        public static final int ic_tab_mail_white=0x7f02000b;
-        public static final int ic_tab_report=0x7f02000c;
-        public static final int ic_tab_report_grey=0x7f02000d;
-        public static final int ic_tab_report_white=0x7f02000e;
+        public static final int androidconsumer=0x7f020000;
+        public static final int cabpoolmapbluepin=0x7f020001;
+        public static final int cabpoolmapredpin=0x7f020002;
+        public static final int cabpoolmapyellowpin=0x7f020003;
+        public static final int ic_tab_artists=0x7f020004;
+        public static final int ic_tab_artists_grey=0x7f020005;
+        public static final int ic_tab_artists_white=0x7f020006;
+        public static final int ic_tab_car=0x7f020007;
+        public static final int ic_tab_car_grey=0x7f020008;
+        public static final int ic_tab_car_white=0x7f020009;
+        public static final int ic_tab_mail=0x7f02000a;
+        public static final int ic_tab_mail_grey=0x7f02000b;
+        public static final int ic_tab_mail_white=0x7f02000c;
+        public static final int ic_tab_report=0x7f02000d;
+        public static final int ic_tab_report_grey=0x7f02000e;
+        public static final int ic_tab_report_white=0x7f02000f;
     }
     public static final class id {
         public static final int ScrollViewIndoorReport=0x7f060008;
