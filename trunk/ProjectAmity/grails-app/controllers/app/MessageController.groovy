7856b0a958c4d37f87c42fb5bd147f7d8f2e65d6
yuanjie91 <yuanjie91@8bc94c53-19bf-512c-9016-643959c5a65d>
2011-03-13 14:37:25 +0000

    Users now get push notifications by email when they have new PMs on Project Amity.

--- trunk/ProjectAmity/grails-app/controllers/app/MessageController.groovy
+++ trunk/ProjectAmity/grails-app/controllers/app/MessageController.groovy
@@ -3,6 +3,7 @@ package app
 import java.text.SimpleDateFormat
 import java.util.Calendar
 import grails.converters.JSON
+import org.apache.commons.mail.*
 
 class MessageController
 {
@@ -174,6 +175,22 @@ class MessageController
 
             if( newMessage.save() )
             {
+                if( recipient.emailConfirm.equalsIgnoreCase("true") )
+                {
+                    Email email = new SimpleEmail()
+                    email.setHostName("smtp.gmail.com")
+                    email.setSmtpPort(587)
+                    email.setAuthenticator(new DefaultAuthenticator("noreply.projectamity@gmail.com", "amity6780"))
+                    email.setTLS(true)
+                    email.setFrom("noreply.projectamity@gmail.com")
+                    email.setSubject("Project Amity - New Message from " + session.user.name)
+                    email.setMsg(session.user.name + " has sent you a message titled:\n\n" +
+                                 "\"" + newMessage.subject + "\"\n\n" +
+                                 "View the message by logging in to Project Amity now!\n\n" +
+                                 "www.projectamity.info")
+                    email.addTo(recipient.email)
+                    email.send();
+                }
                 render 'T'
             }
             else
@@ -260,116 +277,6 @@ class MessageController
         params.messageModuleUnreadMessages = messageCheckingService.getUnreadMessages(session.user)
         params.totalResults = sentMessages.totalCount
         [sentMessages : sentMessages, params : params]
-    }
-
-    // view a message
-    def view =
-    {
-        session.user = Resident.get(7)
-
-        def messageToView = Message.findById(params.id)
-
-        if(messageToView == null)
-        {
-            params.messageModuleUnreadMessages = messageCheckingService.getUnreadMessages(session.user)
-            flash.messageOperationStatus = '<br/><p>The message you are trying to view does not exist!</p>'
-            redirect(action: 'index')
-        }
-        else if( session.user != messageToView.sender && session.user != messageToView.receiver )
-        {
-            params.messageModuleUnreadMessages = messageCheckingService.getUnreadMessages(session.user)
-            flash.messageOperationStatus = '<br/><p>You are not authorised to view this message!</p>'
-            redirect(action: 'index')
-        }
-        else if( session.user != messageToView.sender )
-        {
-            messageToView.isRead = true
-            params.messageModuleUnreadMessages = messageCheckingService.getUnreadMessages(session.user)
-            [message : messageToView, params : params]
-        }
-        else
-        {
-            params.messageModuleUnreadMessages = messageCheckingService.getUnreadMessages(session.user)
-            [message : messageToView, params : params]
-        }
-    }
-
-    // closure to call before create.gsp is loaded.
-    def create =
-    {
-        params.messageModuleUnreadMessages = messageCheckingService.getUnreadMessages(session.user)
-        [params : params]
-    }
-
-    // action that is triggered once user chooses to send a message
-    def send =
-    {
-        def errors = ''
-        def recipient
-        def subject
-        def message
-
-        // Check if recipient exists
-        recipient = params.receiverUserID
-        if( Resident.findByUserid(recipient) == null )
-        {
-            errors += '<li>A recipient by the name of ' + recipient + ' does not exist!<br/>Are you sure you have typed the recipient\'s User ID correctly?'
-        }
-        else
-        {
-            recipient = Resident.findByUserid(recipient)
-        }
-
-        // Check if the subject is specified.
-        if( !params.subject.trim().equals("") )
-        {
-            subject = params.subject
-        }
-        else
-        {
-            errors += '<li>You did not specify the subject of the message.</li>'
-        }
-
-        // Check if the message is specified.
-        if( !params.message.trim().equals("") )
-        {
-            message = params.message
-        }
-        else
-        {
-            errors += '<li>You cannot send an empty message.</li>'
-        }
-
-        if( errors.length() > 0 )
-        {
-            errors = '<p>Some Errors Occured!</p><ul>' + errors
-            errors += '</ul>'
-            params.errors = errors
-            // If there are missing fields, throw back the error message.
-            render(view: 'create', params : params )
-        }
-        else
-        {
-            def newMessage = new Message()
-            newMessage.sender = session.user
-            newMessage.receiver = recipient
-            newMessage.subject = removeHTML(subject)
-            newMessage.message = removeHTML(message)
-            newMessage.timeStamp = new Date()
-            newMessage.isRead = false
-
-            if( newMessage.save() )
-            {
-                flash.messageOperationStatus = '<br/><p>Your message has been sent!</p>'
-                redirect( action: 'index' )
-            }
-            else
-            {
-                println( newMessage.errors )
-                params.errors = '<p>An error occured when sending your message. Would you like to try again?</p>'
-                render(view: 'create', params : params )
-            }
-        }
     }
 
 }
